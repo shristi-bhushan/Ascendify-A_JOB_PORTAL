3ccc0c1dceb454a96923ce2cfc77e485daee87df
shristi-bhushan <shristibhushan2001@gmail.com>
2023-06-08 00:59:53 +0530

    Added find job page

--- Ascendify/Admin/ContactList.aspx.cs
+++ Ascendify/Admin/ContactList.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +12,74 @@ namespace Ascendify.Admin
 {
     public partial class ContactList : System.Web.UI.Page
     {
+        SqlConnection con;
+        SqlCommand cmd;
+        DataTable dt;
+        string str = ConfigurationManager.ConnectionStrings["cs"].ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Session["admin"] == null)
+            {
+                Response.Redirect("../User/Login.aspx");
+            }
 
+            if (!IsPostBack)
+            {
+                ShowContact();
+            }
+        }
+
+        private void ShowContact()
+        {
+            string query = string.Empty;
+            con = new SqlConnection(str);
+            query = @"Select Row_Number() over(Order by (Select 1)) as [Sr.No], ContactId, Name, Email, Subject, Message from Contact";
+            cmd = new SqlCommand(query, con);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            dt = new DataTable();
+            sda.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex = e.NewPageIndex;
+            ShowContact();
+
+        }
+
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            try
+            {
+                GridViewRow row = GridView1.Rows[e.RowIndex];
+                int contactId = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
+                con = new SqlConnection(str);
+                cmd = new SqlCommand("Delete from Contact where ContactId = @id", con);
+                cmd.Parameters.AddWithValue("id", contactId);
+                con.Open();
+                int r = cmd.ExecuteNonQuery();
+                if (r > 0)
+                {
+                    lblMsg.Text = "Job deleted successfully!";
+                    lblMsg.CssClass = "alert alert-success";
+
+                }
+                else
+                {
+                    lblMsg.Text = "Cannot delete this record!";
+                    lblMsg.CssClass = "alert alert-danger";
+                }
+                con.Close();
+                GridView1.EditIndex = -1;
+                ShowContact();
+            }
+            catch (Exception ex)
+            {
+                con.Close();
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
         }
     }
 }
